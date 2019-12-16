2897e9eac131230f605e17ccf8caeb0ad66cf6e3
Joseda8 <jd.montoya.s8@gmail.com>
2019-12-16 16:44:26 -0600

    Cambios menores

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  private urlapi = 'https://api-sensor.herokuapp.com/hello/w';
+  title = 'SensorApp';
+  public currentEuroRates: any = null;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.getCurrentEuroRates();
+  }
+
+  private getCurrentEuroRates() {
+    const currencies = 'USD,GBP,CHF,JPY';
+    const url = `${this.urlapi}?symbols=${currencies}`;
+    this.httpClient
+      .get(url, {responseType: 'text'})
+      .subscribe(apiData => (this.currentEuroRates = apiData));
+  }
+
+//      .subscribe(data => console.log(data))
+
+}
